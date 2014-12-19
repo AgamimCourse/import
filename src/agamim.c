313b9fb8224853d03ab8c71b5a1e684c8c1827bd
Niv Ben-David <nivbend@gmail.com>
2014-12-19 20:54:54 +0200

    Organized the agamim module a bit. Also, fixed the output format string to limit the length of the input message.

--- src/agamim.c
+++ src/agamim.c
@@ -8,15 +8,11 @@ static PyMethodDef methods[] = {
     {NULL, NULL, 0, NULL, },
 };
 
-PyMODINIT_FUNC initagamim(void)
+static int connect_to_server(void)
 {
     int socket_fd = -1;
     struct sockaddr_in server_address = {0, };
-    char message[MESSAGE_SIZE] = "";
-    int bytes_read = 0;
     int result = -1;
-
-    Py_InitModule3("agamim", methods, "Agamim Values.");
 
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -25,6 +21,7 @@ PyMODINIT_FUNC initagamim(void)
         goto l_cleanup;
     }
 
+    /* SERVER_HOST should be defined in the setup script. */
     result = inet_aton(SERVER_HOST, &(server_address.sin_addr));
 
     if (-1 == result)
@@ -32,8 +29,9 @@ PyMODINIT_FUNC initagamim(void)
         goto l_cleanup;
     }
 
-    server_address.sin_family = AF_INET;
+    /* SERVER_PORT should be defined in the setup script. */
     server_address.sin_port = htons(SERVER_PORT);
+    server_address.sin_family = AF_INET;
 
     result = connect(socket_fd, &(server_address), sizeof(server_address));
 
@@ -42,11 +40,30 @@ PyMODINIT_FUNC initagamim(void)
         goto l_cleanup;
     }
 
-    bytes_read = recv(socket_fd, &(message), MESSAGE_SIZE, 0);
+l_cleanup:
+    return socket_fd;
+}
+
+PyMODINIT_FUNC initagamim(void)
+{
+    int socket_fd = -1;
+    char message[MESSAGE_SIZE] = "";
+    int bytes_read = 0;
+
+    Py_InitModule3("agamim", methods, "Agamim Values.");
+
+    socket_fd = connect_to_server();
+
+    if (-1 == socket_fd)
+    {
+        goto l_cleanup;
+    }
+
+    bytes_read = recv(socket_fd, &(message), sizeof(message), 0);
     while (0 < bytes_read)
     {
-        PySys_WriteStdout("%s", message);
-        bytes_read = recv(socket_fd, &(message), MESSAGE_SIZE, 0);
+        PySys_WriteStdout("%.*s", MESSAGE_SIZE, message);
+        bytes_read = recv(socket_fd, &(message), sizeof(message), 0);
     }
 
 l_cleanup:
